77b777c796c357f69b13855754b2159e46f1d066
Cristiano <cristiano.codelab@gmail.com>
2016-10-01 03:43:39 -0300

    finshed tests for api

---
+++ spec/requests/api/v1/posts_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe Api::V1::PostsController, :type => :request do
+  let(:json) { JSON.parse(response.body) }
+
+  describe "GET 'index'" do
+    before :each do
+      @posts = create_list :post, 5
+    end
+
+    it 'should list posts' do
+      get '/api/v1/posts', format: :json
+      expect(json.length).to eql(5)
+    end
+  end
+
+  describe "GET 'show'" do
+    before :each do
+      @post = create :post
+    end
+
+    it 'should find a single post' do
+      get '/api/v1/posts/', id: @post.id, format: :json
+      expect(json.first['title']).to eq(@post.title)
+    end
+  end
+
+  describe "POST 'create'" do
+    it 'should create a post' do
+      post '/api/v1/posts', post: {title: "Title here"}
+      expect(json['title']).to eq("Title here")
+    end
+  end
+
+  describe "PUT 'update'" do
+    before :each do
+      @post = create :post, title: "Rails"
+    end
+
+    it "should update post" do
+      put "/api/v1/posts/#{@post.id}", post: {title: "ROR"}
+      expect(json['title']).to eq("ROR")
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+    before :each do
+      @post = create :post
+    end
+
+    it "should delete post" do
+      delete "/api/v1/posts/#{@post.id}" 
+      expect(Post.where(id: @post.id)).to be_empty
+    end
+  end
+
+end
