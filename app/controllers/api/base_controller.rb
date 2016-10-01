085b652facbe2091b6623b4d32b08212a61e7e5f
Cristiano <cristiano.codelab@gmail.com>
2016-10-01 14:26:37 -0300

    added cors methods to api

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,27 @@
 class Api::BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors
+  
   respond_to :json
+  
+  private 
+    def cors
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token, responseType'
+    end
+
+    def cors_preflight_check
+      if request.method == 'OPTIONS'
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'Content-Type, X-Requested-With, X-Prototype-Version, x-auth-token, responseType'
+        headers['Access-Control-Max-Age'] = '1728000'
+
+        render :text => '', :content_type => 'text/plain'
+      end
+    end
+
 end
